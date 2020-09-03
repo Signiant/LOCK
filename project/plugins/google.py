849542716e7ccea26a2a5f42fc73598c0f287a9d
Mike Perttula <mperttula@signiant.com>
2020-09-03 15:29:28 -0400

    Azure and google enhancements (#13)
    * Fix bad variable
    
    * Add secret manager update
    
    * Add rolling update
    
    * Update google requirements
    
    * Remove extra comments
    
    * Make more generic
    
    * Remove requirment for full module

---
+++ project/plugins/google.py
@@ -0,0 +1,130 @@
+"""
+command line application and sample code for accessing a secret version.
+"""
+
+import logging
+import httplib2
+import pprint
+import time
+import sys
+from google.oauth2 import service_account
+from google.cloud import secretmanager
+from datetime import datetime
+from googleapiclient.discovery import build
+from oauth2client.service_account import ServiceAccountCredentials
+
+logging.getLogger('googleapiclient.discovery_cache').setLevel(logging.CRITICAL)
+
+def set_secret_manager(configMap, username,  **key_args):
+    auth = configMap['Global']['google_credentials']['client_cred']
+    # Create the Secret Manager client.
+    try:
+        client = secretmanager.SecretManagerServiceClient.from_service_account_json(auth)
+    except Exception as e:
+        logging.error("Error: {0}".format(e))
+        return
+    
+    from project import values
+    if values.DryRun is True:
+        logging.info('Dry run ')
+    else:
+        # Build the resource name of the parent secret.
+        try: 
+            parent_access = client.secret_path(key_args.get('project_id'), key_args.get('key_name'))
+            payload_access = values.access_key[0].encode('UTF-8')
+            response = client.add_secret_version(parent_access, {'data': payload_access})
+            logging.debug("Response: {0}".format(response))
+        except Exception as e:
+            logging.error("     ***** Exception trying to update Google Access Key - Key may need to be updated manually. Exception: {0}".format(e))
+            return
+        try: 
+            parent_secret = client.secret_path(key_args.get('project_id'), key_args.get('key_secret'))
+            payload_secret = values.access_key[1].encode('UTF-8')
+            response = client.add_secret_version(parent_secret, {'data': payload_secret})
+            logging.debug("Response: {0}".format(response))
+        except Exception as e:
+            logging.error("     ***** Exception trying to update Google Secret Key - Key may need to be updated manually. Exception: {0}".format(e))
+            return
+
+        logging.info("      Access key and Secret key written to Secret Manager")
+        pass
+
+
+def wait_for_operation(compute, project, region, operation):
+    logging.info("      Starting rolling update for {0}".format(project))
+    while True:
+        result = compute.regionOperations().get(
+            project=project,
+            region=region,
+            operation=operation).execute()
+
+        if result['status'] == 'DONE':
+            logging.info('      Done')
+            if 'error' in result:
+                raise Exception(result['error'])
+            return result
+
+        time.sleep(5)
+
+def rotate_instance_groups(configMap, username,  **key_args):
+    auth = configMap['Global']['google_credentials']['client_cred']
+
+    credentials = service_account.Credentials.from_service_account_file(auth)
+    #authenticate with compute api
+    try:
+        compute = build('compute', 'v1', credentials=credentials)
+    except Exception as e:
+        logging.error("     ***** Exception trying to authenticate with google: {0}".format(e))
+        return
+    logging.debug("Authorized")
+    regions = key_args.get('regions')
+    for item in regions:
+        region = list(item)[0]
+        project = item[region]['project']
+        instance_group = item[region]['instance_group']
+        logging.debug("Project: {0} Instance_group {1} Region {2}".format(project, instance_group, region))
+
+        #retrieve instance template in use from project (this case is 1 regional instance manager in project)
+        try:
+            project_list = compute.regionInstanceGroupManagers().list(
+                project=project,
+                region=region).execute()
+            instance_template = project_list['items'][0]['versions'][0]['instanceTemplate']
+        except Exception as e:
+            logging.error("     ***** Unable to retrieve current instance template: {0}".format(e))
+            return 
+        logging.debug("template: {0}".format(project_list['items'][0]['versions'][0]['instanceTemplate']))
+        #set version to current datetime
+        version = str(datetime.now())
+        #create body for patch update
+        body = {   
+            "updatePolicy": {
+                "minimalAction": "REPLACE",
+                "type": "PROACTIVE",
+                "maxSurge": {
+                    "fixed": 0
+                },
+                "maxUnavailable": { 
+                    "fixed": 2
+                },
+                "minReadySec": 300,
+                "replacementMethod": "recreate"
+            },
+            "versions": [
+                {
+                "instanceTemplate": instance_template,
+                "name": version
+                }
+            ]
+        }
+        #run rolling update to get new keys
+        try:
+            operation = compute.regionInstanceGroupManagers().patch(
+                project=project,
+                region=region,
+                instanceGroupManager=instance_group,
+                body=body).execute()
+            wait_for_operation(compute, project, region, operation['name'])
+        except Exception as e:
+            logging.error("     ***** Unable to update instance group: {0}".format(e))
+            return 
