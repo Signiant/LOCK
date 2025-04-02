f00a64397afa3ef1899de1a92eae9e70fab28392
Grant Dioguardi <gdioguardi@signiant.com>
2025-04-02 11:18:36 -0400

    Implement multi-threading for validate and list

--- project/main.py
+++ project/main.py
@@ -19,6 +19,74 @@ import yaml
 from project.plugins import iam
 from project.plugins.iam import validate_new_key, get_new_key, create_and_test_key, delete_older_key, get_iam_client
 from project import values
+from project import utils
+
+
+def validate_keys_for_user(userdata, configMap, username):
+    username_to_validate = (next(iter(userdata)))
+    if username != "all" and username != username_to_validate:
+        return
+    user_data = userdata.get(username_to_validate)
+    if user_data.get('plugins'):
+        if user_data.get('plugins')[0].get('iam'):
+            if 'get_new_key' in user_data.get('plugins')[0].get('iam')[0]:
+                validate_new_key(configMap, username_to_validate, user_data)
+            else:
+                logging.info(
+                    f'   No get_new_key section for iam plugin for user {username_to_validate} - skipping')
+        else:
+            logging.info(f'   No iam plugin section for user {username_to_validate} - skipping')
+    else:
+        logging.info(f'   No plugins section for user {username_to_validate} - skipping')
+
+
+def validate_keys(username, all_users, configMap):
+    utils.run_threads(all_users, validate_keys_for_user, configMap, username)
+
+
+def rotate_update(config_map, user_data, username, ssh_username=None, ssh_password=None):
+    update_access_key(('', ''))
+    modules = user_data['plugins']
+    for plugin in modules:
+        my_plugin = importlib.import_module('project.plugins.' + list(plugin.keys())[0])
+        plugin = plugin.get(list(plugin.keys())[0])
+        for method in plugin:  # modules = dict, module = str
+            key_args = (method[list(method.keys())[0]])  # get key pair of method to run
+            if key_args is None:
+                key_args = {}
+            if ssh_username:
+                key_args['ssh_user'] = ssh_username
+            if ssh_password:
+                key_args['ssh_password'] = ssh_password
+            method_to_call = getattr(my_plugin, list(method.keys())[0])  # get method name to run
+            logging.info("Running "+str(method_to_call)[:-15].lstrip('<') + "for " + username)
+            result = method_to_call(config_map, username, **key_args)
+            # TODO: Check result and abort remaining methods if one fails
+            if 'get_new_key' in str(method_to_call) and not result:
+                logging.error(f'Failed to get new key - skipping {username}')
+                return
+
+
+def rotate_keys(username, all_users, configMap, user_data, ssh_username, ssh_password):
+    if username == 'all':
+        for user_data in all_users:
+            username = (next(iter(user_data)))
+            user_data = user_data.get(username)
+            rotate_update(configMap, user_data, username, ssh_username, ssh_password)
+    else:
+        rotate_update(configMap, user_data, username, ssh_username, ssh_password)
+
+
+def list_keys_for_user(user_data, configMap):
+    username = (next(iter(user_data)))
+    iam.list_keys(configMap, username)
+
+
+def list_keys(username, all_users, configMap):
+    if username == 'all':
+        utils.run_threads(all_users, list_keys_for_user, configMap)
+    else:
+        iam.list_keys(configMap, username)
 
 
 def update_access_key(key):
@@ -108,7 +176,7 @@ def main():
         print('DEBUG logging requested')
         log_level = logging.DEBUG
 
-    logFormatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    logFormatter = logging.Formatter('%(asctime)s - %(levelname)s: (tid=%(thread)d) [%(module)s] %(message)s')
     rootLogger = logging.getLogger()
     rootLogger.setLevel(logging.DEBUG)
     consoleHandler = logging.StreamHandler()
@@ -156,6 +224,7 @@ def main():
             ssh_password = input(f"Password for {args.ssh_username}: ")
 
     logging.debug(f"Config file {str(configMap)}")
+    user_data = None
     all_users = configMap['Users']
     for userdata in all_users:
         if username == (next(iter(userdata))):
@@ -170,80 +239,11 @@ def main():
         update_access_key(args.key)
 
     if args.action == 'list':
-        key_args = {}
-        # client = get_iam_client(configMap, **key_args)
-        if username == 'all':
-            for user_data in all_users:
-                username = (next(iter(user_data)))
-                iam.list_keys(configMap, username)
-        else:
-            iam.list_keys(configMap, username)
-
+        list_keys(username, all_users, configMap)
     elif args.action == 'rotate':  # run functions listed in the config file.
-        if username == 'all':
-            for user_data in all_users:
-                username = (next(iter(user_data)))
-                user_data = user_data.get(username)
-                rotate_update(configMap, user_data, username, args.ssh_username, ssh_password)
-        else:
-            rotate_update(configMap, user_data, username, args.ssh_username, ssh_password)
-
+        rotate_keys(username, all_users, configMap, user_data, args.ssh_username, ssh_password)
     elif args.action == 'validate':  # validate that new key is being used and delete the old unused key
-        if username == 'all':
-            for userdata in all_users:
-                username_to_validate = (next(iter(userdata)))
-                user_data = userdata.get(username_to_validate)
-                if user_data.get('plugins'):
-                    if user_data.get('plugins')[0].get('iam'):
-                        if 'get_new_key' in user_data.get('plugins')[0].get('iam')[0]:
-                            validate_new_key(configMap, username_to_validate, user_data)
-                        else:
-                            logging.info(f'   No get_new_key section for iam plugin for user {username_to_validate} - skipping')
-                    else:
-                        logging.info(f'   No iam plugin section for user {username_to_validate} - skipping')
-                else:
-                    logging.info(f'   No plugins section for user {username_to_validate} - skipping')
-        else:
-            for userdata in all_users:
-                username_to_validate = (next(iter(userdata)))
-                if username == username_to_validate:
-                    user_data = userdata.get(username_to_validate)
-                    if user_data.get('plugins'):
-                        if user_data.get('plugins')[0].get('iam'):
-                            if 'get_new_key' in user_data.get('plugins')[0].get('iam')[0]:
-                                validate_new_key(configMap, username_to_validate, user_data)
-                            else:
-                                logging.info(f'   No get_new_key section for iam plugin for user {username_to_validate} - skipping')
-                        else:
-                            logging.info(f'   No iam plugin section for user {username_to_validate} - skipping')
-                    else:
-                        logging.info(f'   No plugins section for user {username_to_validate} - skipping')
-
-
-def rotate_update(config_map, user_data, username, ssh_username=None, ssh_password=None):
-    update_access_key(('', ''))
-    modules = user_data['plugins']
-    try:
-        for plugin in modules:
-            my_plugin = importlib.import_module('project.plugins.' + list(plugin.keys())[0])
-            plugin = plugin.get(list(plugin.keys())[0])
-            for method in plugin:  # modules = dict, module = str
-                key_args = (method[list(method.keys())[0]])  # get key pair of method to run
-                if key_args is None:
-                    key_args = {}
-                if ssh_username:
-                    key_args['ssh_user'] = ssh_username
-                if ssh_password:
-                    key_args['ssh_password'] = ssh_password
-                method_to_call = getattr(my_plugin, list(method.keys())[0])  # get method name to run
-                logging.info("Running "+str(method_to_call)[:-15].lstrip('<') + "for " + username)
-                result = method_to_call(config_map, username, **key_args)
-                # TODO: Check result and abort remaining methods if one fails
-                if 'get_new_key' in str(method_to_call) and not result:
-                    logging.error(f'Failed to get new key - skipping {username}')
-                    raise StopIteration
-    except StopIteration:
-        pass
+        validate_keys(username, all_users, configMap)
 
 
 if __name__ == "__main__":
