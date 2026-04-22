f383a67ab33cc83a343bce64fad89707d16bf083
Grant Dioguardi <132940460+gdioguardi@users.noreply.github.com>
2026-04-22 13:05:43 -0400

    Stop using SSH to rotate secrets in Azure VMs and fix 1Password/GCP issues (#42)
    * Cycle out instances as opposed to changing secrets using SSH for Azure
    
    * Replace usage of 'list' 1Password SDK call with 'list_all'
    
    * Set maxSurge value to 3
    
    * Fix validation for 'rotate_ses_smtp_user' action
    
    ---------
    
    Co-authored-by: Grant Dioguardi <gdioguardi@signiant.com>

--- project/plugins/1password.py
+++ project/plugins/1password.py
@@ -97,7 +97,7 @@ def create_item(
 
 def get_item_id(client: Client, vault_id: str, item_title: str) -> str:
     item_id = None
-    items = asyncio.run(client.items.list(vault_id))
+    items = asyncio.run(client.items.list_all(vault_id)).obj
     for item in items:
         if item.title == item_title:
             item_id = item.id
@@ -107,7 +107,7 @@ def get_item_id(client: Client, vault_id: str, item_title: str) -> str:
 
 def get_vault_id(client: Client, vault_title: str) -> str:
     vault_id = None
-    vaults = asyncio.run(client.vaults.list())
+    vaults = asyncio.run(client.vaults.list_all()).obj
     for vault in vaults:
         if vault.title == vault_title:
             vault_id = vault.id
